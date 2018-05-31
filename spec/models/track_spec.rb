79a9bc728ee81578af4ba92831885cdf410da9d3
frizbee <mail2andrewa@gmail.com>
2018-05-31 18:56:20 +0700

    add specs to check track county exists

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Track, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @car = FactoryBot.create :track }
+  subject { @car }
+
+  it { should respond_to :country }
 end
