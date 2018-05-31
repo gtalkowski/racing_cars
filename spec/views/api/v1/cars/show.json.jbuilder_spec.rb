8f06c285ce4cf606e0f771941021524c56ecf39b
frizbee <mail2andrewa@gmail.com>
2018-05-31 22:16:42 +0700

    rfactor spec view show.json.jbuilder

--- spec/views/api/v1/cars/show.json.jbuilder_spec.rb
+++ spec/views/api/v1/cars/show.json.jbuilder_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe "api/v1/cars/show", type: :view do
   before { @car = FactoryBot.create :car }
 
+  let(:json) { JSON.parse(response.body) }
+
   it "renders" do
     render
     expect(response).not_to be_empty
@@ -10,22 +12,22 @@ RSpec.describe "api/v1/cars/show", type: :view do
 
   it "show data car_slug attribute" do
     render
-    expect(JSON.parse(response)["data"]["car"]["car_slug"]).to eq(@car.car_slug)
+    expect(json["data"]["car"]["car_slug"]).to eq(@car.car_slug)
   end
 
   it "should show max_speed_on_track data" do
     render
-    expect(JSON.parse(response)["data"]["car"]["max_speed_on_track"]).not_to be_nil
+    expect(json["data"]["car"]["max_speed_on_track"]).not_to be_nil
   end
 
   it "should show max_speed" do
     render
-    expect(JSON.parse(response)["data"]["car"]["max_speed"]).not_to be_nil
+    expect(json["data"]["car"]["max_speed"]).not_to be_nil
   end
 
   it "should show max_speed with km/h" do
     render
-    expect(JSON.parse(response)["data"]["car"]["max_speed"]).to eq "#{@car.max_speed}km/h"
+    expect(json["data"]["car"]["max_speed"]).to eq "#{@car.max_speed}km/h"
   end
 
 end
