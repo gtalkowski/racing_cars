5dd81cdfd6e825ee029957448471176c48ebf8ab
frizbee <mail2andrewa@gmail.com>
2018-05-31 23:00:32 +0700

    added slowin_factor surface in speed_calcualtion

--- app/models/speed_calculation.rb
+++ app/models/speed_calculation.rb
@@ -1,11 +1,21 @@
 class SpeedCalculation
 
+  SURFACE_TYPE = {:show => 35, :gravel => 12, :asphalt => 0}
+
   def initialize(car_speed, track)
     @car_speed = car_speed
     @track = track
   end
 
   def max_speed
-    "#{@car_speed.to_i - 30}"
+    "#{@car_speed.to_i - slowing_factor_surface.to_i}"
+  end
+
+  def slowing_factor_surface
+    30
+  end
+
+  def track
+    Track.find_by(name: @track.downcase)
   end
 end
