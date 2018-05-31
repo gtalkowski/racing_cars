50f27bf72beab765eee1a836145734dc76109338
frizbee <mail2andrewa@gmail.com>
2018-05-31 18:59:50 +0700

    added more track respond to specs

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -5,4 +5,7 @@ RSpec.describe Track, type: :model do
   subject { @car }
 
   it { should respond_to :country }
+  it { should respond_to :name }
+  it { should respond_to :timezone }
+  it { should respond_to :surface_type }
 end
