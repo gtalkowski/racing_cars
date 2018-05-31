a23507ce3a599bc3f57586d2667737cff0190599
frizbee <mail2andrewa@gmail.com>
2018-05-31 14:34:21 +0700

    rspec get show with car object

--- spec/controllers/api/v1/car_controller_spec.rb
+++ spec/controllers/api/v1/car_controller_spec.rb
@@ -3,9 +3,19 @@ require 'rails_helper'
 RSpec.describe Api::V1::CarController, type: :controller do
 
   describe "GET #show" do
+
+    before(:each) do
+      @car = FactoryBot.create :car
+    end
+
     it "returns http success" do
       get :show
       expect(response).to have_http_status(:success)
+    end
+
+    it "returns with car slug" do
+      get :show, id: @car.car_slug
+      expect(JSON.parse(response.body)["car_slug"]).to eq @car.car_slug
     end
   end
 
