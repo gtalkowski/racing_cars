e30337f2fd0e5622036d9df06ff5145730ba605e
frizbee <mail2andrewa@gmail.com>
2018-05-31 23:31:34 +0700

    fix error with nil track, added track create before each test

--- spec/models/speed_calculation_spec.rb
+++ spec/models/speed_calculation_spec.rb
@@ -11,12 +11,20 @@ RSpec.describe SpeedCalculation, type: :model do
 
   describe "Surface factor" do
 
+    before(:each) do
+      track
+    end
+
     it "shouldn't be nil" do
       expect(speed.max_speed).not_to be_nil
     end
 
     it "should get max_speed less that 280" do
       expect(speed.max_speed.to_i).to be < 280
+    end
+
+    it "display max_speed_on_track as 182" do
+      expect(speed.max_speed.to_i).to eq 182
     end
   end
 end
