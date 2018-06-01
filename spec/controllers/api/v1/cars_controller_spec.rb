3300c517a8473f3e40961dcd203faa32c43c8143
frizbee <mail2andrewa@gmail.com>
2018-06-01 23:55:34 +0700

    spec refactor json parse data

--- spec/controllers/api/v1/cars_controller_spec.rb
+++ spec/controllers/api/v1/cars_controller_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Api::V1::CarsController, type: :controller do
 
   render_views
 
-  let(:json) { JSON.parse(response.body) }
+  let(:json) { JSON.parse(response.body)["data"]["car"] }
 
   describe "GET #show" do
 
@@ -25,19 +25,19 @@ RSpec.describe Api::V1::CarsController, type: :controller do
 
     it "returns with car slug" do
       get :show, :id => @car.car_slug
-      expect(json["data"]["car"]["car_slug"]).to eq @car.car_slug
+      expect(json["car_slug"]).to eq @car.car_slug
     end
 
     it "should show 'no track selected' if no track params specified" do
       params = {id: @car.car_slug, track: nil }
       get :show, params
-      expect(json["data"]["car"]["max_speed_on_track"]).to eq "no track selected"
+      expect(json["max_speed_on_track"]).to eq "no track selected"
     end
 
     it "should have track name" do
       params = { id: @car.car_slug, :track => "nurburgring" }
       get :show, params
-      expect(json["data"]["car"]["max_speed_on_track"].to_i).to be < @car.max_speed.to_i
+      expect(json["max_speed_on_track"].to_i).to be < @car.max_speed.to_i
     end
   end
 
